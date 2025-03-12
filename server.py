c91d4fa3efbe560f502ccce709c25f9d4c183c83
Hazz-i <wahidh697@gmail.com>
2025-03-12 17:49:05 +0700

    add: server.py

--- server.py
+++ server.py
@@ -1,8 +1,19 @@
-import flask 
-import os
-import json
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 import requests
+import json
 import time
+
+import os
 import dotenv
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+CORS = CORS(app, origins="*")
+
+@app.route("/")
+def index():
+    return "Alamakk, wes running to?"
+
+if __name__ == "__main__":
+    app.run(debug=True)
